ed0dc727c7ae98cc7c53d7de91f0d9a0084071b0
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2023-05-18 21:26:39 -0500

    Add confetti to winner

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import confetti from 'canvas-confetti';
 
 const TURNS = {
   X: 'x',
@@ -71,6 +72,7 @@ function App() {
 
     const newWinner = checkWinner(newBoard)
     if (newWinner) {
+      confetti();
       setWinner(newWinner);
     } else if (checkEndGame(newBoard)) {
       setWinner(false);
